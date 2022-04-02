22a8053a75e522135c15884e932ccc2371784c11
tomoka-itoh <tomokaitoh@163.com>
2022-04-02 21:56:51 +0800

    修改登录界面

---
+++ login/js/loginPage.js
@@ -0,0 +1,32 @@
+function login(){
+    alert("hello! " + document.getElementById("userName_input").value);
+}
+
+function register(){
+    alert("hello! " + document.getElementById("userName_input").value);
+}
+
+function toRegister(){
+    document.getElementById("window_title").innerHTML = "注册";
+    document.getElementById("password2_lable").style.display = "inline-block";
+    document.getElementById("password2_input").style.display = "inline-block";
+    document.getElementById("loginButton").style.display = "none";
+    document.getElementById("registerButton").style.display = "block";
+    document.getElementById("loginTip").style.display = "none";
+    document.getElementById("registerTip").style.display = ""; 
+}
+
+function toLogin(){
+    document.getElementById("window_title").innerHTML = "登录";
+    document.getElementById("password2_lable").style.display = "none";
+    document.getElementById("password2_input").style.display = "none";
+    document.getElementById("registerButton").style.display = "none";
+    document.getElementById("loginButton").style.display = "block";
+    document.getElementById("loginTip").style.display = "";
+    document.getElementById("registerTip").style.display = "none"; 
+
+}
+
+function toFindPassword(){
+
+}
